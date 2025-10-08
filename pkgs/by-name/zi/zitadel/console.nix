ac601f8ead4e54c5813ea07272c1277f7c4ba2e0
Mohamed Laradji <mlaradji@proton.me>
2025-10-08 11:49:49 -0400

    zitadel: 2.71.7 -> 4.3.0

--- pkgs/by-name/zi/zitadel/console.nix
+++ pkgs/by-name/zi/zitadel/console.nix
@@ -5,58 +5,129 @@
 }:
 
 {
+  lib,
   stdenv,
-  fetchYarnDeps,
-  yarnConfigHook,
-  yarnBuildHook,
+  pnpm,
   nodejs,
-
   grpc-gateway,
+  protoc-gen-connect-go,
   protoc-gen-grpc-web,
-  protoc-gen-js,
+  buf,
+  fetchFromGitHub,
+  pkg-config,
+  protobuf_27,
+  fetchPnpmDeps,
+  pnpmConfigHook,
+  protoc-gen-js
 }:
 
 let
   protobufGenerated = generateProtobufCode {
     pname = "zitadel-console";
-    inherit version;
     nativeBuildInputs = [
       grpc-gateway
+      protoc-gen-connect-go
       protoc-gen-grpc-web
       protoc-gen-js
     ];
     workDir = "console";
     bufArgs = "../proto --include-imports --include-wkt";
     outputPath = "src/app/proto";
-    hash = "sha256-UzmwUUYg0my3noAQNtlUEBQ+K6GVnBSkWj4CzoaoLKw=";
+    hash = "sha256-A78to2uARzVvXLMxvZpvm7CLhZM45DaDycGjX/ZvBus=";
   };
+
+  zitadelProtobufGenerated = generateProtobufCode {
+    pname = "zitadel-proto";
+    workDir = "packages/zitadel-proto";
+    bufArgs = "../../proto";
+    outputPath = ".";
+    hash = "sha256-baPS1wbsUQzelI55zswTPvX7ojXbPts967lwgcZGvlE=";
+  };
+
+  client = stdenv.mkDerivation (finalAttrs: {
+    pname = "zitadel-client";
+    inherit version;
+    src = zitadelRepo;
+
+    pnpmDeps = fetchPnpmDeps {
+      inherit (finalAttrs) pname version src;
+      fetcherVersion = 4;
+      hash = "sha256-3CBJpCcQYp6UzIecE7vQK+iqbhmfrESaH6dVf0yRMzU=";
+    };
+
+    pnpmWorkspaces = [
+      "@zitadel/proto"
+      "@zitadel/client"
+    ];
+
+    nativeBuildInputs = [
+      pnpmConfigHook
+      nodejs
+      buf
+      pnpm
+    ];
+
+    preBuild = ''
+      cp -r ${zitadelProtobufGenerated}/{cjs,es,types} packages/zitadel-proto
+    '';
+
+    buildPhase = ''
+      runHook preBuild
+      pnpm --filter=@zitadel/client run build
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      cp -r packages/zitadel-client/dist "$out"
+      runHook postInstall
+    '';
+  });
 in
-stdenv.mkDerivation {
+stdenv.mkDerivation (finalAttrs: {
   pname = "zitadel-console";
   inherit version;
 
   src = zitadelRepo;
 
-  sourceRoot = "${zitadelRepo.name}/console";
-
-  offlineCache = fetchYarnDeps {
-    yarnLock = "${zitadelRepo}/console/yarn.lock";
-    hash = "sha256-ekgLd5DTOBZWuT63QnTjx40ZYvLKZh+FXCn+h5vj9qQ=";
+  pnpmDeps = fetchPnpmDeps {
+    inherit (finalAttrs) pname version src;
+    fetcherVersion = 4;
+    hash = "sha256-3CBJpCcQYp6UzIecE7vQK+iqbhmfrESaH6dVf0yRMzU=";
   };
 
+  pnpmWorkspaces = [
+    "@zitadel/proto"
+    "@zitadel/client"
+    "console"
+  ];
+
   nativeBuildInputs = [
-    yarnConfigHook
-    yarnBuildHook
+    pnpmConfigHook
     nodejs
+    buf
+    pnpm
   ];
 
   preBuild = ''
-    cp -r ${protobufGenerated} src/app/proto
+    cp -r ${protobufGenerated} console/src/app/proto
+    cp -r ${zitadelProtobufGenerated}/{cjs,es,types} packages/zitadel-proto
+    cp -r ${client} packages/zitadel-client/dist
+  '';
+
+  buildPhase = ''
+    runHook preBuild
+
+    pnpm --filter=console build
+
+    runHook postBuild
   '';
 
   installPhase = ''
     runHook preInstall
-    cp -r dist/console "$out"
+
+    cp -r console/dist/console "$out"
+
     runHook postInstall
   '';
-}
+})
